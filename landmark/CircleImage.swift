a5e6b7b079a814a4c7b0cc30526f7dc5d17aab80
ryan-hwang-00 <dlsrb07@gmail.com>
2021-10-21 14:46:19 +0900

    chapter 1 done

---
+++ landmark/CircleImage.swift
@@ -0,0 +1,23 @@
+//
+//  CircleImage.swift
+//  landmark
+//
+//  Created by 황인규 on 10/21/21.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var body: some View {
+        Image("turtlerock")
+            .clipShape(Circle())
+            .overlay(Circle().stroke(Color.white, lineWidth: 4))
+            .shadow(radius: 7)
+    }
+}
+
+struct CircleImage_Previews: PreviewProvider {
+    static var previews: some View {
+        CircleImage()
+    }
+}
