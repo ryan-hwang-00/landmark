6d564aacecc6eb52d0d12e88f9cb316e7fa25536
ingyu hwang <dlsrb07@gmail.com>
2021-10-22 13:40:10 +0900

    목록 동적으로 만들기

---
+++ landmark/Views/LandmarkList.swift
@@ -0,0 +1,20 @@
+//
+//  LandmarkList.swift
+//  landmark
+//
+//  Created by 황인규 on 10/21/21.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LandmarkList_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkList()
+    }
+}
